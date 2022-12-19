cc19f3101701ce597d1c414f5e8a45ccb744ed1d
dspindler <daniel.spindler@leebmann.de>
2022-12-19 19:14:34 +0100

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,28 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import Script from "next/Script";
 
 
 export default function MyApp({Component, pageProps}: AppProps) {
 
     return (
-        <main >
-            <Component {...pageProps} />
-        </main>
+        <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-FBSHT9404G"
+                strategy="afterInteractive"/>
+            <Script>
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-FBSHT9404G');
+                `}
+            </Script>
+            <main>
+                <Component {...pageProps} />
+            </main>
+        </>
     )
 }
 
